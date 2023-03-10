13e1da06be07d020ce7c6512b8aa1798c469e55e
BushraFatimaBF <vglampard@protonmail.com>
2023-03-10 16:57:10 +0000

    skelton of input and output and dictionaries put in place

---
+++ src/constants/alphabet.js
@@ -0,0 +1,7 @@
+export const ALPHABET = [
+    'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+]
+
+export const ALPHABET_SHUFFLE = [
+    'P','Q','i','j','k','l','C','G','m','n','o','L','M','p','s','D','E','F','t','u','N','O','w','x','y','K','S','T','z','A','b','H','I','J','U','R','v','a','h','V','W','X','B','c','d','q','r','e','f','g','Y','Z',
+]
