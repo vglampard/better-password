e1ade69ac887681cf42d8fbfd1e799a6a7bad73b
BushraFatimaBF <vglampard@protonmail.com>
2023-03-15 17:14:36 +0000

    added conditional formatting to password display

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ function App() {
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
 
-
   const STATES = {
     input,
     setInput,
--- src/Output/Output.js
+++ src/Output/Output.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Output({password}) {
   return (
-    <div>password: {password}</div>
+    <div> {password ? "Your better password:" + password : ""}</div>
   )
 }
