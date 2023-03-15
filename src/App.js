198acfaf9eb736840a9e689b11c5c68b4f82e11c
BushraFatimaBF <vglampard@protonmail.com>
2023-03-15 00:48:12 +0000

    playing around with window.navigator

--- src/App.js
+++ src/App.js
@@ -25,11 +25,36 @@ arr.splice(1, 0, "$");
 setPassword(arr.join(''));
 }
 
+const options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0,
+};
+
+function success(pos) {
+  const crd = pos.coords;
+
+  console.log("Your current position is:");
+  console.log(`Latitude : ${crd.latitude}`);
+  console.log(`Longitude: ${crd.longitude}`);
+  console.log(`More or less ${crd.accuracy} meters.`);
+}
+
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
+
+
+
+console.log(window.navigator.geolocation.getCurrentPosition(success, error, options))
+
   return (
     <div className="App">
       <header className="App-header">
        <Input STATES={STATES} handleClick={()=>handleClick(input, salt)}/>
        <Output password={STATES.password}/>
+       <button onClick = {()=>navigator.clipboard.writeText(password)}  >Copy to clipboard</button>
       </header>
     </div>
   );
